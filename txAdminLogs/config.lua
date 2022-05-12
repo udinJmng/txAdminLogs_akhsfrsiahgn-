c1138026580c7e62caef3dfffddae1f3c47bb094
Dustine_XXX <86789381+udinJmng@users.noreply.github.com>
2022-05-12 22:18:56 +0700

    Update config.lua

--- txAdminLogs/config.lua
+++ txAdminLogs/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
-Config.txAdminWebhook = 'WEBHOOK'
+Config.txAdminWebhook = 'https://discord.com/api/webhooks/974329756013174915/qbGWi1tY33T_DbjDt1MtNMbSeFaMeRgwtD_8fDwI7AGXDb8XfVP1nRjC9800rI9tRnEB'
 
-Config.Username = 'txAdmin Logs'
+Config.Username = 'UJ4NG OFFICER'
 
 Config.FilterAnnouncements = true
